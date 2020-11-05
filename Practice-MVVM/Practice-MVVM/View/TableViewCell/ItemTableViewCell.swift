0ebed6ec408cfaec99462f08a5b99fee809b403d
key5771 <ksj57715@hanmail.net>
2020-11-05 18:07:03 +0900

    Create Project & TableView, TableViewCell

---
+++ Practice-MVVM/Practice-MVVM/View/TableViewCell/ItemTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ItemTableViewCell.swift
+//  Practice-MVVM
+//
+//  Created by 김기현 on 2020/11/05.
+//
+
+import UIKit
+
+class ItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
